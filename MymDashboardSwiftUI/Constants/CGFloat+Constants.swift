2946f21d8b2bd1158162343af3002d156ad85dc3
Róbert Oravec <robertoravec@Roberts-MacBook-Pro-2.local>
2020-12-29 17:17:40 +0100

    Created empty placeholder

--- MymDashboardSwiftUI/Constants/CGFloat+Constants.swift
+++ MymDashboardSwiftUI/Constants/CGFloat+Constants.swift
@@ -4,4 +4,5 @@ extension CGFloat {
     
     static var kPaddingS: CGFloat { 10 }
     static var kPaddingL: CGFloat { 16 }
+    static var kCornerRadius: CGFloat { 11 }
 }
--- MymDashboardSwiftUI/View/DashboardPlaceholderView.swift
+++ MymDashboardSwiftUI/View/DashboardPlaceholderView.swift
@@ -1,14 +1,43 @@
 import SwiftUI
+import Rswift
 
 struct DashboardPlaceholderView: View {
     
+    let titleText: String
+    let bottomText: String
+    let backgroundImage: ImageResource
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            backgroundImage.image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            VStack(alignment: .leading) {
+                Text(titleText)
+                    .foregroundColor(R.color.basicWhiteBlack.color)
+                    .font(.title3)
+                Spacer()
+                Text(bottomText)
+                    .foregroundColor(R.color.basicWhiteBlack.color)
+                    .font(.footnote)
+            }
+            .padding(.kPaddingL)
+        }
+        .frame(height: 260)
+        .background(R.color.roundedCell.color)
+        .cornerRadius(.kCornerRadius)
+        .padding(EdgeInsets(top: 0, leading: .kPaddingL, bottom: 0, trailing: .kPaddingL))
     }
 }
 
 struct DashboardPlaceholderView_Previews: PreviewProvider {
     static var previews: some View {
-        DashboardPlaceholderView()
+        DashboardPlaceholderView(
+            titleText: "Title",
+            bottomText: "Longer subtitle",
+            backgroundImage: R.image.no_events_today
+        )
+        .frame(height: 400)
+        .background(R.color.appBackground.color)
     }
 }
